0d860b2a9f99076ea27b3d80a8197c1254b943e8
joshl26 <joshlehman.dev@gmail.com>
2022-12-26 16:28:43 -0500

    commit

--- index.js
+++ index.js
@@ -3,6 +3,35 @@ console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
 
+// function sumPrices(first, second, third = 0) {
+//   return first + second + third;
+// }
+
+// function sumPrices(...numbers) {
+//   return numbers.reduce((total, val) => {
+//     return total + (Number.isNaN(Number(val)) ? 0 : Number(val));
+//   });
+// }
+
+let names = ["Hat", "Boots", "Gloves"];
+
+let prices = [];
+
+prices.push(100);
+prices.push("100");
+prices.push(50.25);
+
+console.log(`First Item: ${names[0]}: ${prices[0]}`);
+
+let sumPrices = (...numbers) => {
+  return numbers.reduce((total, val) => {
+    return total + (Number.isNaN(Number(val)) ? 0 : Number(val));
+  });
+};
+
+let totalPrice = sumPrices(...prices);
+console.log(`Total: ${totalPrice}: ${typeof totalPrice}`);
+
 let taxRate; //no tax rate has been defined
 console.log(`Tax rate: ${taxRate ?? 10}%`);
 taxRate = 0; //zero-rated for tax
@@ -14,9 +43,17 @@ if (hatPrice === bootsPrice) {
   console.log("Prices are different");
 }
 
-let totalPrice = Number(hatPrice) + Number(bootsPrice);
+// let totalPrice = sumPrices(hatPrice, bootsPrice);
 
-console.log(`Total Price: ${totalPrice}`);
+// // let totalPrice = Number(hatPrice) + Number(bootsPrice);
+
+// console.log(`Total Price: ${totalPrice} ${typeof totalPrice}`);
+
+// totalPrice = sumPrices(100, 200, 300);
+// console.log(`Total Price: ${totalPrice} ${typeof totalPrice}`);
+
+// totalPrice = sumPrices(100, 200, undefined, false, "hello");
+// console.log(`Total Price: ${totalPrice} ${typeof totalPrice}`);
 
 let myVariable = "Adam";
 
