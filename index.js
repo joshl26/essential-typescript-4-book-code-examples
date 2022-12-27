85738d388812c33c33439685fe002607d3c40e7b
joshl26 <joshlehman.dev@gmail.com>
2022-12-27 10:05:07 -0500

    commit

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 let hat = {
   name: "Hat",
-  price: 100,
+  _price: 100,
+  priceIncTax: 100 * 1.2,
+
+  set price(newPrice) {
+    this._price = newPrice;
+    this.priceIncTax = this._price * 1.2;
+  },
+
+  get price() {
+    return this._price;
+  },
+
+  // writeDetails: function () {
+  //   console.log(`${this.name}: ${this.price}, ${this.priceIncTax}`);
+  // },
+
+  writeDetails: () =>  {
+    console.log(`${this.name}: ${this.price}, ${this.priceIncTax}`);
+  },
+
+
 };
 
 let boots = {
   name: "Boots",
   price: "100",
+
+  get priceIncTax() {
+    return Number(this.price) * 1.2;
+  },
 };
+
+hat.writeDetails();
+hat.price = 120;
+hat.writeDetails();
+
+console.log(`Hat: ${hat.price}, ${hat.priceIncTax}`);
+hat.price = 120;
+console.log(`Hat: ${hat.price}, ${hat.priceIncTax}`);
+
+console.log(`Boot: ${boots.price}, ${boots.priceIncTax}`);
+boots.price = 120;
+console.log(`Hat: ${boots.price}, ${boots.priceIncTax}`);
 
 let additionalProperties = {
   ...hat,
