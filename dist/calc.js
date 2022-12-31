5524d0325a37d881fad5dea596dcff54ac1cd7c6
joshl26 <joshlehman.dev@gmail.com>
2022-12-30 23:20:22 -0500

    commit

--- dist/calc.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sum = void 0;
-function sum(...vals) {
-    return vals.reduce((total, val) => total += val);
-}
-exports.sum = sum;
