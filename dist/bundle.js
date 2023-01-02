b15139a2c15299c0f06df77ca12c8b4898a8ba36
joshl26 <joshlehman.dev@gmail.com>
2023-01-02 11:43:26 -0500

    commit

---
+++ dist/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+console.log("Web app");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b2RvX2pvc2gvLi9zcmMvaW5kZXgudHMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJXZWIgYXBwXCIpO1xyXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.ts",
+  output: { filename: "bundle.js" },
+  resolve: { extensions: [".ts", ".js"] },
+  module: {
+    rules: [{ test: /\.ts/, use: "ts-loader", exclude: /node_modules/ }],
+  },
+};
