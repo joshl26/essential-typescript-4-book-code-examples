d25193dbceaa2f75f62fa94e0c624c7c73fcbdb7
joshl26 <joshlehman.dev@gmail.com>
2023-01-01 22:13:35 -0500

    commit

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,7 @@
 declare function calculateTax(amount: number): number;
 declare function writePrice(product: string, price: number): void;
+declare let prices: number[];
+declare let names: string[];
 declare let hatPrice: number;
 declare let glovesPrice: number;
 declare let umbrellaPrice: number;
