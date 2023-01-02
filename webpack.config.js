fc5e9cac3d60f9da2184fb02cb68d125024677eb
joshl26 <joshlehman.dev@gmail.com>
2023-01-02 15:24:19 -0500

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,12 @@
-const webpack = require("webpack");
-const path = require("path");
-
 module.exports = {
   mode: "development",
   devtool: "inline-source-map",
   entry: "./src/index.ts",
   output: { filename: "bundle.js" },
-  resolve: { extensions: [".ts", ".js", ".css"] },
+  resolve: { extensions: [".ts", ".tsx", ".js", ".css"] },
   module: {
     rules: [
-      { test: /\.ts/, use: "ts-loader", exclude: /node_modules/ },
+      { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
     ],
   },
